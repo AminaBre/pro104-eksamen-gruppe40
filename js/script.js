0c192ba87ce7e8afab7a746e68c7667c570472a6
Jan-Andreas Rusnak <j4n4ndre4sr@gmail.com>
2020-05-22 16:32:41 +0200

    fiksa pil og scroll til knapp

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
     });
 
     $('#arrowBtn').click(function(){
-        $('html, body').animate({scrollTop : 0}, 300);
+        $('html, body').animate({scrollTop : 0}, 0);
     });
 });
 
