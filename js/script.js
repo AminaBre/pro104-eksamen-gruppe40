6324c1c6d2c4f2984cd36db5542fe46229691ce1
Jan-Andreas Rusnak <j4n4ndre4sr@gmail.com>
2020-05-25 16:11:38 +0200

    asdklfj

--- js/script.js
+++ js/script.js
@@ -269,3 +269,20 @@ function onLeave()
 		this.style.backgroundColor = normalColor;
 	}
 }
+
+//QA
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function () {
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            //accordion is open, we need to close it
+            content.style.maxHeight = null;
+        } else {
+            //accordion is closed
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
