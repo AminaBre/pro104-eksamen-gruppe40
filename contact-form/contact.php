1003f20f00bd4550a957e3f6715a9a4f9a0c3280
AminaBre <aminahb@live.no>
2020-05-26 13:24:38 +0200

    DRAG AND DROP
    Drag and drop, plassering endret på alle objekter. Slettet visuelle bokser som ikke brukes. Slettet kodeblokk i js som ikke var i bruk.

--- contact-form/contact.php
+++ contact-form/contact.php
@@ -1,79 +1,75 @@
 <?php
-/**
- * CONFIGURE ERRTAAANG HERE
+/*
+ *  CONFIGURE EVERYTHING HERE
  */
 
- // an email that will be in the "From" field of the email
- $from = 'Demo contact form <demo@domain.com>';
+// an email address that will be in the From field of the email.
+$from = 'Demo contact form <demo@domain.com>';
 
- // an email that will recive the email with the output of the form
- $sendTo = 'Demo contact form <demo@domain.com>';
+// an email address that will receive the email with the output of the form
+$sendTo = 'Demo contact form <demo@domain.com>';
 
- //subject of the email we are reciving
- $subject 'New message from contact form';
+// subject of the email
+$subject = 'New message from contact form';
 
- // form names and their respective translations
- // array variable name => Text to appear in the mail!
- $fields = array(
-                'name' => 'Name', 
-                'surname' => 'Surname',
-                'phone' => 'Phone',
-                'email' => 'Email'
-                'message' => 'Message'
- );
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
 
- //message displaying that everything is okey and sound!
- $okMessage = 'Thank you for submitting! We will probably never reply since this is a test';
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
- //Error message if something goes wrong
- $errorMessage = 'ERRRRRRRRR, something went wrong my guy... Pls try again l8r h8r ;)'
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
- //turning off debugging since this is a exam and i had to learn PHP myself.... MIKE!
- error_reporting(E_ALL & ~ E_NOTICE);
+/*
+ *  LET'S DO THE SENDING
+ */
 
- /*
-  * La oss gjøre litt sånn sending a! aka lets do the sending 
-  */
-  try
-  {
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
 
-      if count($_POST) == 0) throw new \Exception('Form is empty');
-      $emailText = "You have a new message from your contact form \n============\n";
+try
+{
 
-      foreach ($_POST as $key => $value) {
-          //if the field exsists in the $fields array, include it in the mail :)
-          if(isset($fields[$key])){
-              $emailText .= "$fields[$key]: $value\n";
-          }
-      }
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+            
+    $emailText = "You have a new message from your contact form\n=============================\n";
 
-      //All of the neccessary headers for the mail yuhhh
-      $headers = array('Content-Type: text/plain; charset="UTF-8";',
-                        'From ' .$from,
-                        'Reply-To: ' .$from,
-                        'Return-Path: ' .$from
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
     );
-
-    //Send the mail!
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-    $reponseArray = array('type' => 'success', 'message' => $okMessage);
     
-  }
-  catch (\Exception $e)
-  {
-      $reponseArray = array('type' => 'danger', 'message' => $errorMessage);
-  }
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
-  // if requested by the AJAX request, this retuns a JSON respone
-  if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
 
     header('Content-Type: application/json');
 
     echo $encoded;
 }
-
-//else: just display the message
+// else just display the message
 else {
     echo $responseArray['message'];
 }
--- js/script.js
+++ js/script.js
@@ -21,67 +21,13 @@ $(document).ready(function () {
 	});
 });
 
+<<<<<<< HEAD
+// INPUT BOKSER
+=======
 // KOM-I-GANG-BOKSER
 
-//!Drag and drop test start
-/* The dragging code for '.draggable' from the demo above
- * applies to this demo as well so it doesn't have to be repeated. */
 
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-	// only accept elements matching this CSS selector
-	accept: '#yes-drop',
-	// Require a 75% element overlap for a drop to be possible
-	overlap: 0.75,
-
-	// listen for drop related events:
-
-	ondropactivate: function (event) {
-		// add active dropzone feedback
-		event.target.classList.add('drop-active')
-	},
-	ondragenter: function (event) {
-		var draggableElement = event.relatedTarget
-		var dropzoneElement = event.target
-
-		// feedback the possibility of a drop
-		dropzoneElement.classList.add('drop-target')
-		draggableElement.classList.add('can-drop')
-		draggableElement.textContent = 'Dragged in'
-	},
-	ondragleave: function (event) {
-		// remove the drop feedback style
-		event.target.classList.remove('drop-target')
-		event.relatedTarget.classList.remove('can-drop')
-		event.relatedTarget.textContent = 'Dragged out'
-	},
-	ondrop: function (event) {
-		event.relatedTarget.textContent = 'Dropped'
-	},
-	ondropdeactivate: function (event) {
-		// remove active dropzone feedback
-		event.target.classList.remove('drop-active')
-		event.target.classList.remove('drop-target')
-	}
-})
-
-interact('.drag-drop')
-	.draggable({
-		inertia: true,
-		modifiers: [
-			interact.modifiers.restrictRect({
-				restriction: 'parent',
-				endOnly: true
-			})
-		],
-		autoScroll: true,
-		// dragMoveListener from the dragging demo above
-		listeners: { move: dragMoveListener }
-	})
-
-//!Drag and drop test slutt
-
-
+>>>>>>> 266b59ba89548510eb86e1ddea8097a14b7cc932
 //Legg til Ansatt
 function addMember(event) {
 	event.preventDefault();
@@ -321,4 +267,52 @@ for (var i = 0; i < accordions.length; i++) {
 			content.style.maxHeight = content.scrollHeight + "px";
 		}
 	}
+}
+
+
+
+// DRAG AND DROP
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
 }
